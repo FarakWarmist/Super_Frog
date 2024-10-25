b2a07499a1d65cd67b72e475d9bbaf89ed92e53d
William Sylvestre <farakwarts@gmail.com>
2024-10-25 16:19:05 -0400

    Add Cam follow Player to Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,28 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Camera playerCam;
     Rigidbody2D rb;
     public float speed = 5f;
     public float jumpPower = 100f;
     SpriteRenderer spriteRenderer;
 
-
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        playerCam = Camera.main;
     }
 
     private void Update()
     {
+        var camPos = playerCam.transform.position;
+        camPos.x = transform.position.x;
+        playerCam.transform.position = camPos;
+
         float x = Input.GetAxis("Horizontal");
         var velocity = rb.velocity;
         velocity.x = speed * x;
